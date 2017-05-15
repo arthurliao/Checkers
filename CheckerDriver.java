1f651c416b6af4de7955a12e99c53b42744dddd4
jrcarcher <james.ryan.crowley@gmail.com>
2017-05-15 12:37:04 -0400

    Update CheckerDriver.java

--- CheckerDriver.java
+++ CheckerDriver.java
@@ -1,4 +1,45 @@
-
 public class CheckerDriver {
 
+   boolean blackIsPlayer;
+   boolean redIsPlayer;
+   boolean isRedsMove;
+   CheckerBoard board;
+   
+   
+   public static void main(String args[]){
+      Scanner console = new Scanner (System.in);
+      isRedsMove = true;
+      System.out.println("Is black a real player? type y/n");//Determine who is real and who will be computer controlled
+      if(console.next() == "y"){
+         blackIsPlayer = true;
+      }
+      else{
+         blackIsPlayer = false;
+      }
+      System.out.println("Is red a real player? type y/n");
+      if(console.next() == "y"){
+         redIsPlayer = true;
+      }
+      else{
+         redIsPlayer = false;
+      }
+     
+      board = new CheckerBoard();//Create board
+     
+     
+      while(board.checkWin() == false){//Play Game
+      
+        
+        
+        
+        
+      
+      
+      }
+      
+      //Find who won
+      
+      
+      
+   }
 }
