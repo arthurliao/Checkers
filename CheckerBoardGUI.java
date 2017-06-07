4f527b38c4c2b94c9a5eb5c8d3dd43bdfb3cddde
jrcarcher <james.ryan.crowley@gmail.com>
2017-06-06 23:20:50 -0400

    Update CheckerBoardGUI.java

--- CheckerBoardGUI.java
+++ CheckerBoardGUI.java
@@ -1,40 +1,39 @@
- import java.awt.*;
+import java.awt.*;
  import java.awt.event.*;
  import java.util.Scanner;
  import java.util.*;
-
  import javax.swing.*;
 
 public class CheckerBoardGUI extends JPanel{
+
 	private JButton[][] buttonBoard;
-	private JButton reset,start;
-	static boolean blackIsPlayer;
-	static boolean redIsPlayer;
-	static boolean isRedsMove;
+	private JButton reset,start/*,rand*/;
 	static CheckerBoard board;
 	private boolean checker = false;
 	private Coordinates temp;
 	private ArrayList<Move> legalMoves;
-	
+
 	private static StretchIcon redIcon = new StretchIcon("C:\\Users\\red.png");
 	private static StretchIcon blackIcon = new StretchIcon("C:\\Users\\black.jpg");
 	private static StretchIcon redKingIcon = new StretchIcon("C:\\Users\\redKing.png");
 	private static StretchIcon blackKingIcon = new StretchIcon("C:\\Users\\blackKing.jpg");
-	public CheckerBoardGUI(){
-		setLayout(new BorderLayout());
-		reset = new JButton("Reset");
-		start = new JButton("Start");
+	
+   public CheckerBoardGUI(){
+		
+      setLayout(new BorderLayout());
+
+		reset = new JButton("Winner is N/A");
+		start = new JButton("Start");     
+      //rand = new JButton("Random Move");
 		reset.setEnabled(false);
-		
-	    board = new CheckerBoard();//Create board
-		
+	
+      //board = new CheckerBoard();//Create board
 		JPanel south = new JPanel();
 		south.setLayout(new FlowLayout());
 		add(south, BorderLayout.SOUTH);
 		south.add(start);
 		south.add(reset);
 		start.addActionListener(new StartListener());
-		
 		JPanel center = new JPanel();
 		center.setLayout(new GridLayout(8,8));
 		add(center, BorderLayout.CENTER);
@@ -42,7 +41,6 @@ public class CheckerBoardGUI extends JPanel{
 		for(int r = 0; r < 8; r ++){
 			for(int c = 0; c < 8; c ++){
 				buttonBoard[r][c] = new JButton();
-
 				if(r % 2 == 0){
 					if(c % 2 == 1){
 						buttonBoard[r][c].setBackground(Color.red);
@@ -66,18 +64,20 @@ public class CheckerBoardGUI extends JPanel{
 				center.add(buttonBoard[r][c]);
 			}
 		}
-	}//end constructor
-	
+	}//end constructor	
+
 	private class StartListener implements ActionListener{
-		
+
 		public void actionPerformed(ActionEvent e){
+
+        board = new CheckerBoard(); 
 			for(int r = 0; r < 8; r ++){
 				for(int c = 0; c < 8; c ++){
 					if(buttonBoard[r][c].getBackground() == Color.white){
 						buttonBoard[r][c].setEnabled(true);
 						buttonBoard[r][c].addActionListener(new MainListener(r, c));	
+                  buttonBoard[r][c].setIcon(null);
 					}
-					
 					if(r == 0 ||  r == 2){
 						if(c % 2 == 0){
 						    buttonBoard[r][c].setIcon(blackIcon);
@@ -92,7 +92,6 @@ public class CheckerBoardGUI extends JPanel{
 						if(c % 2 == 1){
 						    buttonBoard[r][c].setIcon(redIcon);
 						}
-							
 					}
 					else if(r == 6){
 						if(c % 2 == 0){
@@ -154,7 +153,27 @@ public class CheckerBoardGUI extends JPanel{
 				}
 			}
 			refreshBoard();
-			
+         if(board.checkWin() == true)
+         {
+            if(board.redNum == 0)
+            {
+               reset = new JButton("Winner is Black");
+               for(int r = 0; r < 8; r ++){
+      				for(int c = 0; c < 8; c ++){
+      						buttonBoard[r][c].setEnabled(false);
+                  }
+               }
+            }
+            else
+            {
+               reset = new JButton("Winner is Red");
+               for(int r = 0; r < 8; r ++){
+      				for(int c = 0; c < 8; c ++){
+      						buttonBoard[r][c].setEnabled(false);
+                  }
+               }
+            }
+         }            
 		}
 	}//end MainListener
 	
@@ -189,74 +208,13 @@ public class CheckerBoardGUI extends JPanel{
 			System.out.println();
 		}
 	}
+
 		   public static void main(String args[]){
-		      
-			      
 			  JFrame frame = new JFrame("Checkers");
-			  frame.setSize(450, 400);
-			  frame.setLocation(200, 100);
+			  frame.setSize(850, 800);
+			  frame.setLocation(500, 100);
 			  frame.setContentPane(new CheckerBoardGUI());
 			  frame.setVisible(true);
 			  frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-			   
-			  Scanner console = new Scanner(System.in);
-		      isRedsMove = true;
-		      /*System.out.println("Is black a real player? type y/n");//Determine who is real and who will be computer controlled
-		      if(console.next().equals("y")){
-		         blackIsPlayer = true;
-		      }
-		      else{
-		         blackIsPlayer = false;
-		      }
-		      System.out.println("Is red a real player? type y/n");
-		      if(console.next().equals("y")){
-		         redIsPlayer = true;
-		      }
-		      else{
-		         redIsPlayer = false;
-		      }*/
-		     
-
-		     
-		     
-		      while(board.checkWin() == false){//Play Game
-		         int tempX = 0;
-		         int tempY = 1;
-		         if(isRedsMove == true){
-		            while(board.legalMoves(tempX, tempY).size() == 0)
-		            {
-		            	System.out.println("Red Player, type x coordinate of the piece you want to move");
-			            tempX = console.nextInt();
-			            System.out.println("Red Player, type y coordinate of the piece you want to move");
-			            tempY = console.nextInt();
-			            System.out.println("Moves available to you are as follows");
-			            System.out.println(board.legalMoves(tempX, tempY));////////////////////////////////////////////////////////////////Insert array of moves here
-		            }
-		            System.out.println("Red Player, type number of move you want to make");
-		            board.move(board.legalMoves(tempX, tempY).get(console.nextInt()));//poll());
-		         }
-		         else{
-		            while(board.legalMoves(tempX, tempY).size() == 0)
-		            {
-		            	System.out.println("Black Player, type x coordinate of the piece you want to move");
-			            tempX = console.nextInt();
-			            System.out.println("Black Player, type y coordinate of the piece you want to move");
-			            tempY = console.nextInt();
-			            System.out.println("Moves available to you are as follows");
-			            System.out.println(board.legalMoves(tempX, tempY));////////////////////////////////////////////////////////////////Insert array of moves here
-		            }
-		            System.out.println("Black Player, type number of the move you want to make");
-		            board.move(board.legalMoves(tempX, tempY).get(console.nextInt()));//poll());
-		         }
-		         System.out.println(board);
-		         if(isRedsMove == true)
-		            isRedsMove = false;
-		         else
-		            isRedsMove = true;
-		      }
-		      
-		      
-		      
-	}
+		     }
 }
-
